830de04eb70d5b2c0abcae45a84d7b42e0070b11
Stephan Krauß <stephan.krauss@herden.de>
2014-06-03 10:35:55 +0200

    Grundaufbau Projekt

---
+++ application/start.php
@@ -0,0 +1,17 @@
+<?php
+
+class application_start{
+
+    public function go(){
+        echo 'go';
+
+        return;
+    }
+
+    public function index(){
+        echo 'index';
+
+        return;
+    }
+
+}
