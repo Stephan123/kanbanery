d4b2cd8968c5a1dc0550c6d01429a8839fac5575
Krauß Stephan <info@suppenterrine.de>
2014-07-14 13:03:17 +0200

    Update datenbankController.php

--- application/controller/datenbankController.php
+++ application/controller/datenbankController.php
@@ -1,70 +1,223 @@
 <?php
+/**
+ * Validierung der ankommenden Parameter in einer Action
+ *
+     $data = array(
+        'product_id'    => 'libgd<script>',
+        'component'     => '10', // Fehler
+        'versions'      => '2.0.33',
+        'testscalar'    => array('2', '23', '10', '12'),
+        'testarray'     => '2'
+    );
 
-class datenbankController implements iToolCrud{
-    protected $pimple = null;
-    protected $params = null;
+    $filter = array(
+        'title' => array(
+            'filter' => FILTER_VALIDATE_REGEXP,
+            'required' => true,
+            'options' => array(
+                "regexp" => "#^([a-z]+)$#i"
+            )
+        ),
+        'firstname' => array(
+            'filter' => FILTER_SANITIZE_MAGIC_QUOTES
+        ),
+        'lastname' => array(
+            'filter' => FILTER_SANITIZE_MAGIC_QUOTES,
+            'required' => true
+        ),
+        'company' => array(
+            'filter' => FILTER_SANITIZE_MAGIC_QUOTES
+        ),
+        'offlinekunde' => array(
+            'filter' => FILTER_SANITIZE_NUMBER_INT,
+            'required' => true
+        ),
+        'country' => array(
+            'filter' => FILTER_SANITIZE_NUMBER_INT,
+            'required' => true
+        ),
+        'street' => array(
+            'filter' => FILTER_SANITIZE_MAGIC_QUOTES,
+            'required' => true
+        ),
+        'zip' => array(
+            'filter' => FILTER_SANITIZE_MAGIC_QUOTES,
+            'required' => true
+        ),
+        'city' => array(
+            'filter' => FILTER_SANITIZE_MAGIC_QUOTES,
+            'required' => true
+        ),
+        'email' => array(
+            'filter' => FILTER_SANITIZE_EMAIL,
+            'required' => true
+        ),
+        'password' => array(
+            'filter' => FILTER_SANITIZE_MAGIC_QUOTES,
+            'required' => true
+        ),
+        'phonenumber' => array(
+            'filter' => FILTER_SANITIZE_NUMBER_INT,
+            'required' => true
+        ),
+        'schriftwechsel' => array(
+            'filter' => FILTER_SANITIZE_NUMBER_INT,
+            'required' => true
+        )
+    );
+ *
+ *
+ * @author Stephan Krauss
+ * @date 14.07.14
+ * @file ToolValidate.php
+ * @package tools
+ */
+class nook_ToolValidate
+{
+    protected $data = array();
+    protected $filter = array();
+    protected $validData = array();
 
-    public function __construct(Pimple $pimple){
-        $this->pimple = $pimple;
-        $this->params = Flight::get('params');
+    /**
+     * @param array $data
+     * @return nook_ToolValidate
+     */
+    public function setDataArray(array $data = array())
+    {
+        $this->data = $data;
+
+        if(count($this->data) == 0)
+            return;
+
+        $this->validateArray();
+
+        return $this;
     }
 
-    public function create(){
-
+    /**
+     * @return array
+     */
+    public function getDataArray(){
+        return $this->validData;
     }
 
-    public function read(){
-        try{
+    /**
+     * @param array $filter
+     * @return nook_ToolValidate
+     */
+    public function setFilter(array $filter){
+        $this->filter = $filter;
 
-            // $tabelleLog = new table_log();
-            // $rows = $tabelleLog->many();
+        return $this;
+    }
 
-            $data = array(
-                'product_id'    => 'libgd<script>',
-                'component'     => '10', // Fehler
-                'versions'      => '2.0.33',
-                'testscalar'    => array('2', '23', '10', '12'),
-                'testarray'     => '2'
-            );
+    /**
+     * Überprüfung der Parameter
+     */
+    protected function validateArray()
+    {
+        foreach($this->data as $key => $value){
 
-            $filter = array(
-                'product_id'   => array(
-                    'filter' => FILTER_SANITIZE_ENCODED
-                ),
-                'component'    => array(
-                    'filter'    => FILTER_VALIDATE_INT,
-                    'options'   => array(
-                        'min_range' => 1,
-                        'max_range' => 10
-                    )
-                ),
-                'versions'     => array(
-                    'filter' => FILTER_SANITIZE_ENCODED
-                ),
-                'doesnotexist' => array(
-                    'filter' => FILTER_VALIDATE_INT
-                ),
-                'testscalar'   => array(
-                    'filter' => FILTER_VALIDATE_INT,
-                ),
-                'testarray'    => array(
-                    'filter' => FILTER_VALIDATE_INT,
-                )
-            );
+            $value = trim($value);
 
-            $toolValidate = new toolValidate();
-            $filterResult = $toolValidate
-                ->setFilter($filter)
-                ->setDataArray($data);
-                // ->getFilterResult();
+            if(!$filter = $this->filter[$key]['filter'])
+                throw new Exception('Filter für: '.$key.' nicht vorhanden');
+
+            if(array_key_exists('options', $this->filter[$key])){
+
+                $bedingungen = array();
+                foreach($this->filter[$key]['options'] as $optionKey => $optionValue ){
+                    $bedingungen['options'][$optionKey] = $optionValue;
+                }
+
+                $result = filter_var($value, $filter, $bedingungen);
+                $this->validData[$key] = $result;
+            }
+            else{
+                $result = filter_var($value, $filter);
+                $this->validData[$key] = $result;
+            }
+
+            // filtern fehlgeschlagen
+            if($result === false)
+                throw new nook_Exception("Filter, Parameter '".$key."' falsch");
         }
-        catch(Exception $e){
-            throw $e;
-        }
+
+        // Kontrolle auf Pflicht - Parameter
+        $this->kontrollePflichtParams();
+
+        return;
     }
 
-    public function update(){}
+    /**
+     * Kontrollier ob Pflichtparameter vorhanden sind
+     *
+     * @throws nook_Exception
+     */
+    protected function kontrollePflichtParams(){
+        foreach($this->filter as $paramName => $optionen){
+            if(array_key_exists('required', $optionen)){
+                if($optionen['required'] === true){
+                    if(!array_key_exists($paramName, $this->validData))
+                        throw new nook_Exception("Parameter '".$paramName."' nicht vorhanden");
+                }
+            }
+        }
 
-    public function delete(){}
+        return;
+    }
 
+    /**
+     * @param $property
+     * @param $value
+     * @return nook_ToolValidate
+     * @throws Exception
+     */
+    public function __set($key, $value)
+    {
+        $data = array(
+            $key => $value
+        );
+
+        $this->data[$key] = $value;
+        $this->validateArray($data);
+
+        return $this;
+    }
+
+    /**
+     * @param $property
+     * @return mixed
+     * @throws Exception
+     */
+    public function __get($property)
+    {
+        if (!array_key_exists($property, $this->data)) {
+            throw new Exception('');
+        }
+
+        return $this->data[$property];
+    }
+
+    /**
+     * @param $property
+     * @return bool
+     */
+    public function __isset($property)
+    {
+        return isset($this->data[ $property ]);
+    }
+
+    /**
+     * @param $property
+     * @return ToolValidate
+     */
+    public function __unset($property)
+    {
+        if (isset($this->data[ $property ])) {
+            unset($this->data[ $property ]);
+        }
+
+        return $this;
+    }
 }
